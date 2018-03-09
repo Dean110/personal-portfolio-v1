c365d4aa3f97b83111b96d2e2b8b114ecab6c93e
BenWilliams <deanpbr@gmail.com>
2018-03-09 16:15:32 -0500

    Testing setup

---
+++ spec/js/app.spec.js
@@ -0,0 +1,7 @@
+describe('Portfolio JavaScript Testing', () => {
+	describe('a method that changes an element\'s visability', () => {
+		it('changes an element class from hidden to show', () =>{
+
+		})
+	})
+})
