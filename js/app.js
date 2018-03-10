1f521f2b76bd0e12d727e9391b123ff3ac124d86
BenWilliams <deanpbr@gmail.com>
2018-03-09 22:15:39 -0500

    Figured some stuff out

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 function displayMenu(element){
-	element.className = 'show';
-
+	if(element.length){
+		element.forEach(function(item){
+			if(item.className.includes('hidden')){
+				item.className = item.className.replace('hidden', 'show');
+			} else {
+				item.className = item.className.replace('show', 'hidden');
+			}	
+		});
+	} else {
+		if(element.className.includes('hidden')){
+			element.className = element.className.replace('hidden', 'show');
+		} else {
+			element.className = element.className.replace('show', 'hidden');
+		}
+	}
 };
--- spec/js/app.spec.js
+++ spec/js/app.spec.js
@@ -22,5 +22,33 @@ describe('Portfolio JavaScript Testing', () => {
 			let changedClassName = testDiv.className;
 			expect(changedClassName).toBe('show foo');
 		})
+		it('replaces show with hidden while keeping other classes', () =>{
+			const underTest=displayMenu;
+			let testDiv = document.createElement('div');
+			testDiv.className +='show ';
+			testDiv.className +='foo';
+			
+			underTest(testDiv);
+
+			let changedClassName = testDiv.className;
+			expect(changedClassName).toBe('hidden foo');
+		})
+		it('replaces show with hidden in all elements in a nodelist', ()=>{
+			const underTest=displayMenu;
+			let testDiv1 = document.createElement('div');
+			testDiv1.className +='show ';
+			testDiv1.className +='foo';
+			let testDiv2 = document.createElement('div');
+			testDiv2.className +='show ';
+			testDiv2.className +='foo';
+			document.body.appendChild(testDiv1);
+			document.body.appendChild(testDiv2);
+			testDivs= document.body.querySelectorAll('div.show');
+			
+			underTest(testDivs);
+
+			expect(testDiv1.className).toBe('hidden foo');
+			expect(testDiv2.className).toBe('hidden foo');		
+		})
 	})
 })
